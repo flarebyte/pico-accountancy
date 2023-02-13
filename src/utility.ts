3589718dbceb4d7b04e91d14424ba6464f43d35e
Olivier Huin <olih@users.noreply.github.com>
2023-02-13 19:29:03 +0000

    Uses the string.js dasherize for backward compatability

--- src/utility.ts
+++ src/utility.ts
@@ -45,12 +45,14 @@ const asCsvValue = (value: string | number): string => {
 export const toCSV = (values: (string | number)[]): string =>
   values.map(asCsvValue).join(',');
 
-export const slugify =
-  (splitter: (textToSplit: string) => string[]) => (text: string) =>
-    text === ''
-      ? ''
-      : splitter(text)
-          .map((t) => t.toLowerCase())
-          .join('-');
-const splitBySpace = (text: string): string[] => text.split(' ');
-export const dasherize = (text: string) => slugify(splitBySpace)(text);
+/**
+ * From: https://github.com/jprichardson/string.js
+ * This function also add a dash for uppercase letter
+ */
+export const dasherize = (text: string) =>
+  text
+    .trim()
+    .replace(/[_\s]+/g, '-')
+    .replace(/([A-Z])/g, '-$1')
+    .replace(/-+/g, '-')
+    .toLowerCase();
